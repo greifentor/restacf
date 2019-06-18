1e551c0d93d0359cec0d64bac4c9634dbe397467
ollie <lieshoff@gmx.de>
2019-06-18 22:55:56 +0200

    Corrections.

--- src/rest/acf/Version.java
+++ src/rest/acf/Version.java
@@ -7,11 +7,9 @@
  *
  */
 
-package restacf;
+package rest.acf;
 
-
-import javax.swing.*;
-
+import javax.swing.JOptionPane;
 
 /**
  * Eine Klasse mit der Versionsnummer der Software.
@@ -23,37 +21,37 @@ import javax.swing.*;
 
 public class Version {
 
-    public static final Version INSTANCE = new Version();
+	public static final Version INSTANCE = new Version();
 
-    /*
-     * Generiert eine Instanz der Versionsklasse mit Default-Parametern.
-     */
-    private Version() {
-        super();
-    }
+	/*
+	 * Generiert eine Instanz der Versionsklasse mit Default-Parametern.
+	 */
+	private Version() {
+		super();
+	}
 
-    /**
-     * Liefert die Nummer der Version zur Applikation.
-     *
-     * @return Die Versionsnummer zur Applikation.
-     */
-    public String getVersion() {
-        return "1.1.1";
-    }
+	/**
+	 * Liefert die Nummer der Version zur Applikation.
+	 *
+	 * @return Die Versionsnummer zur Applikation.
+	 */
+	public String getVersion() {
+		return "1.1.1";
+	}
 
-    @Override public String toString() {
-        return this.getVersion();
-    }
+	@Override
+	public String toString() {
+		return this.getVersion();
+	}
 
-    public static void main(String[] args) {
-        System.out.println("RestACF version is " + INSTANCE.getVersion());
-        new Thread(new Runnable() {
-            public void run() {
-                JOptionPane.showMessageDialog(null, "Version of RestACF is: "
-                        + INSTANCE.getVersion(), "RestACF version",
-                        JOptionPane.INFORMATION_MESSAGE);
-            }
-        }).start();
-    }
+	public static void main(String[] args) {
+		System.out.println("RestACF version is " + INSTANCE.getVersion());
+		new Thread(new Runnable() {
+			public void run() {
+				JOptionPane.showMessageDialog(null, "Version of RestACF is: " + INSTANCE.getVersion(),
+						"RestACF version", JOptionPane.INFORMATION_MESSAGE);
+			}
+		}).start();
+	}
 
 }
--- unittests/rest/acf/VersionTest.java
+++ unittests/rest/acf/VersionTest.java
@@ -7,13 +7,11 @@
  *
  */
 
-package restacf;
-
+package rest.acf;
 
 import static org.junit.Assert.*;
 
 import org.junit.*;
-
 
 /**
  * Tests zur Klasse VersionTest.
@@ -25,22 +23,22 @@ import org.junit.*;
 
 public class VersionTest {
 
-    @Test
-    /** Test auf bestehen der statischen Instanz. */
-    public void testInstanceNotNull() {
-        assertNotNull(Version.INSTANCE);
-    }
+	@Test
+	/** Test auf bestehen der statischen Instanz. */
+	public void testInstanceNotNull() {
+		assertNotNull(Version.INSTANCE);
+	}
 
-    @Test
-    /** Test der Methode <TT>getVersion()</TT>. */
-    public void testGetVersion() {
-        assertEquals("1.1.1", Version.INSTANCE.getVersion());
-    }
+	@Test
+	/** Test der Methode <TT>getVersion()</TT>. */
+	public void testGetVersion() {
+		assertEquals("1.1.1", Version.INSTANCE.getVersion());
+	}
 
-    @Test
-    /** Test der Methode <TT>toString()</TT>. */
-    public void testToString() {
-        assertEquals("1.1.1", Version.INSTANCE.toString());
-    }
+	@Test
+	/** Test der Methode <TT>toString()</TT>. */
+	public void testToString() {
+		assertEquals("1.1.1", Version.INSTANCE.toString());
+	}
 
 }
