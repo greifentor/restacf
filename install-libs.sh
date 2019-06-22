1c98a1954fa437aa29b97279e051ae07ff1128c0
ollie <lieshoff@gmx.de>
2019-06-23 00:07:11 +0200

    Completed change to a Maven project and implemented a first test.

--- install-libs.sh
+++ install-libs.sh
@@ -1,2 +1,3 @@
 mvn install:install-file -Dfile=./lib/archimedes-core-1.0.jar -DgroupId=archimedes -DartifactId=core -Dversion=1.0 -Dpackaging=jar
 mvn install:install-file -Dfile=./lib/baccara-1.10.1.jar -DgroupId=de.ollie -DartifactId=baccara -Dversion=1.10.1 -Dpackaging=jar
+mvn install:install-file -Dfile=./lib/corent-1.67.1.jar -DgroupId=de.ollie -DartifactId=corent -Dversion=1.67.1 -Dpackaging=jar
