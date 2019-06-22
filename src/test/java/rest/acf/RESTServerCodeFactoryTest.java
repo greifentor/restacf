dc2f879950d6d654185c1473748d9e7144d2e600
ollie <lieshoff@gmx.de>
2019-06-23 00:06:39 +0200

    Completed change to a Maven project and implemented a first test.

--- src/test/java/rest/acf/RESTServerCodeFactoryTest.java
+++ src/test/java/rest/acf/RESTServerCodeFactoryTest.java
@@ -1,5 +1,38 @@
 package rest.acf;
 
+import static org.junit.Assert.assertThat;
+import static org.mockito.Mockito.mock;
+
+import static org.hamcrest.Matchers.sameInstance;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.runners.MockitoJUnitRunner;
+
+import baccara.gui.GUIBundle;
+
+/**
+ * Unit tests of class "RESTServerCodeFactory".
+ * 
+ * @author ollie
+ *
+ */
+@RunWith(MockitoJUnitRunner.class)
 public class RESTServerCodeFactoryTest {
+
+	@InjectMocks
+	private RESTServerCodeFactory unitUnderTest;
+
+	@Test
+	public void setGUIBundle_PassAGUIBundle_SetsTheGUIBundleCorrectly() {
+		// Prepare
+		GUIBundle expected = mock(GUIBundle.class);
+		// Run
+		this.unitUnderTest.setGUIBundle(expected);
+		// Check
+		GUIBundle returned = this.unitUnderTest.getGUIBundle();
+		assertThat(returned, sameInstance(expected));
+	}
 
 }
